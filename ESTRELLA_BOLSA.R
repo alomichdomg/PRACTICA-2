bf1d3cea9f4ead04a0410665dd44fb67c89982c0
mtz-mel <amartinez445@alumnos.uaq.mx>
2025-03-09 16:50:47 -0600

    nuevo

--- ESTRELLA_BOLSA.R
+++ ESTRELLA_BOLSA.R
@@ -3,18 +3,18 @@
 
 ### tabla de abundancias: 
 
-abundancias_estrella <- data.frame( 
+abundancias_estrella_BD <- data.frame( 
   especie = c("azul oscuro", "verde claro", "verde menta", "azul claro", 
               "azul cielo", "rosa mexicano", "amarillo", "rosa claro", "naranja",
               "rojo", "rosa", "morado"),
   numero_de_individuos = c(17, 15, 10, 4, 4, 3, 3, 2, 2, 1, 1, 1)
-))
+)
 
 View(abundancias_estrella)
 
 library(ggplot2)
 
-ggplot(abundancias_estrella, aes(x = reorder(especie, -numero_de_individuos), 
+ggplot(abundancias_estrella_BD, aes(x = reorder(especie, -numero_de_individuos), 
                                  y = numero_de_individuos, 
                                  fill = especie)) +
   geom_bar(stat = "identity") +
@@ -36,5 +36,28 @@ ggplot(abundancias_estrella, aes(x = reorder(especie, -numero_de_individuos),
     "morado" = "purple"
   )) +
   theme(axis.text.x = element_text(angle = 90))
+
+source("FUNCIONES.R") #para llamar las funciones
+
+abundancias_estrella <- c(17, 15, 10, 4, 4, 3, 3, 2, 2, 1, 1, 1)
+
+#Shannon:
+E_shannon <- shannon_mejor(abundancias_estrella)
+print(E_shannon)
+
+#Pielou
+E_pielou <- pielou(abundancias_estrella)
+print(E_pielou)
+
+#Simpson
+E_simpson <- simpson_funcion(abundancias_estrella)
+print(E_simpson)
+
+#Simpson inverso
+E_inverso <- inverso_simpson(abundancias_estrella)
+print(E_inverso)
+
+
+
 
 
