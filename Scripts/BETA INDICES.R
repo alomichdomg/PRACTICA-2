f78583567b7b73a2256d6e6101fbd8889b9bbe9d
alomichdomg <adominguez09@alumnos.uaq.mx>
2025-03-09 22:01:44 -0600

    beta 2

--- Scripts/BETA INDICES.R
+++ Scripts/BETA INDICES.R
@@ -5,7 +5,8 @@ datos_beta <- read.csv("DATOS/beta - Hoja 1.csv")
 # TIENE QUE SER UN DATA FRAME
 datos_beta_1 <- as.data.frame((datos_beta > 0) * 1)
 
-datos_beta_1[] <- lapply(datos_beta_pa, as.numeric) #HACER LOS DATOS NUMERICOS
+datos_beta_1[] <- lapply(datos_beta_pa, as.numeric) #HACER LOS DATOS NUMERICOS, para que 
+#los detecte
 
 # Calcular índice de Jaccard
 jaccard_distancia <- vegdist(datos_beta_1, method = "jaccard")
@@ -14,3 +15,4 @@ print(jaccard_distancia)
 #Bray-Curtis:
 bray_distancia<- vegdist(datos_beta_1, method = "bray")
 print(bray_distancia)
+#########
