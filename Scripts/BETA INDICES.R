201f30a6a8511b824ece6e972341d85d6b2696e5
alomichdomg <adominguez09@alumnos.uaq.mx>
2025-03-10 11:01:53 -0600

    .

--- Scripts/BETA INDICES.R
+++ Scripts/BETA INDICES.R
@@ -6,8 +6,6 @@ datos_beta <- read.csv("DATOS/beta - Hoja 1.csv")
 # TIENE QUE SER UN DATA FRAME
 datos_beta_1 <- as.data.frame((datos_beta > 0) * 1)
 
-datos_beta_1[] <- lapply(datos_beta_1, as.numeric) #HACER LOS DATOS NUMERICOS, para que 
-#los detecte
 
 datos_beta_1[ ,-1] #ver datos 
 
