ecd2f9a8e1289dd3d6693a9c45c5a3896c971aab
alomichdomg <adominguez09@alumnos.uaq.mx>
2025-03-09 20:22:38 -0600

    cambios

--- Scripts/LUNA_BOLSA.R
+++ Scripts/LUNA_BOLSA.R
@@ -110,21 +110,5 @@ library(ggplot2)
 naranja_nombres <- c("naranja")
 naranaja_abundancias <- c(77)
 
-#JACCARD:
-#define Jaccard Similarity function
-jaccard <- function(a, b) {
-  intersection = length(intersect(a, b))
-  union = length(a) + length(b) - intersection
-  return (intersection/union)
-}
-
-#find Jaccard Similarity between the two sets 
-jaccard(luna_conjunto, naranja_nombres) #tiene que ser con los nombres de cada cosa.
-
-#METODO CON LA MATRIZ DE DISTANCIAS:
-df<-data.frame(luna_abundancias, naranaja_abundancias) #NO.
-vegdist(df, method = "jaccard")
 
 
-##################################################################
-#rank abundance:
